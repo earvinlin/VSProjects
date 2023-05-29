efc72c5434627bc1adcb8ffc466db4ed78e39617
earvin(vivopc) <lin.earvin@gmail.com(vivopc)>
2023-05-29 11:26:39 +0800

    20230529-1126 Modified file.

--- WindowsProgramming4th/ch02/test_tmain.c
+++ WindowsProgramming4th/ch02/test_tmain.c
@@ -1,9 +1,18 @@
-#include <windows.h>
-#include <tchar.h>
-#include <stdio.h>
+#include "INCLUDE\Everything.h"
+//#include <windows.h>
+//#include <tchar.h>
+//#include <stdio.h>
 
 int _tmain(int argc, LPTSTR argv[])
 {
+    HANDLE hInFile;
+    BOOL DashS;
+    int iArg, iFirstFile;
+
     printf("Hllo world!");
+    iFirstFile = Options(argc, argv, _T("s"), &DashS, NULL);
+    hInFile = CreateFile(argv[iArg], GENERIC_READ, 0, NULL, OPEN_EXISTING,
+    FILE_ATTRIBUTE_NORMAL, NULL);
+    
     return 0;
 }
