56fec435a6038369f9063913bcc12f4068fa3e69
lin.earvin(hpenvynb) <lin.earvin(hpenvbnb)>
2025-05-26 22:55:22 +0800

    20250526-2255 add files.

--- WindowsProgramming4th/test/ReportError/ReportError.c
+++ WindowsProgramming4th/test/ReportError/ReportError.c
@@ -1,40 +1,55 @@
 #include "Everything.h"
 
-/*
-	回報系統錯誤的公用函式
-	取得錯誤碼並轉換為系統錯誤訊息
-	在標準錯誤設備上顯示此訊息以及使用者指定的訊息
-	userMessage		  : 要顯示到訊息以及使用者指定的訊息
-	exitCode		  : 0   - 返回 ; 
-						> 0 - 以此碼呼叫ExitProcess
-	printErrorMessage : 如果設定則顯示最後的系統錯誤訊息
-	※ 編譯指令 ※
-	CL /IC:\WORKSPACES\GithubProjects\VSProjects\WindowsProgramming4th\INCLUDE ReportError.c
-	CL /ID:\WORKSPACES\GithubProjects\VSProjects\WindowsProgramming4th\INCLUDE ReportError.c
- */
 VOID ReportError(LPCTSTR userMessage, DWORD exitCode, BOOL printErrorMessage)
+
+/* General-purpose function for reporting system errors.
+	Obtain the error number and convert it to the system error message.
+	Display this information and the user-specified message to the standard error device.
+	userMessage:		Message to be displayed to standard error device.
+	exitCode:		0 - Return.
+					> 0 - ExitProcess with this code.
+	printErrorMessage:	Display the last system error message if this flag is set. */
 {
 	DWORD eMsgLen, errNum = GetLastError();
 	LPTSTR lpvSysMsg;
-
 	_ftprintf(stderr, _T("%s\n"), userMessage);
 	if (printErrorMessage) {
-		eMsgLen = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER |
-			FORMAT_MESSAGE_FROM_SYSTEM,
-			NULL, errNum,
-			MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		eMsgLen = FormatMessage(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM,
+			NULL, errNum, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
 			(LPTSTR)&lpvSysMsg, 0, NULL);
-		if (eMsgLen > 0) {
-			_fprintf(stderr, _T("%s\n"), lpvSysMsg);
+		if (eMsgLen > 0)
+		{
+			_ftprintf(stderr, _T("%s\n"), lpvSysMsg);
 		}
-		else {
-			_fprintf(stderr, _T("Last Error Number; %d.\n"), errNum);
+		else
+		{
+			_ftprintf(stderr, _T("Last Error Number; %d.\n"), errNum);
 		}
-		if (lpvSysMsg != NULL)
-			LocalFree(lpvSysMsg);
+
+		if (lpvSysMsg != NULL) LocalFree(lpvSysMsg); /* Explained in Chapter 5. */
 	}
+
 	if (exitCode > 0)
 		ExitProcess(exitCode);
+
+	return;
+}
+
+/* Extension of ReportError to generate a user exception
+	code rather than terminating the process. */
+
+VOID ReportException(LPCTSTR userMessage, DWORD exceptionCode)
+
+/* Report as a non-fatal error.
+	Print the system error message only if the message is non-null. */
+{
+	if (lstrlen(userMessage) > 0)
+		ReportError(userMessage, 0, TRUE);
+	/* If fatal, raise an exception. */
+
+	if (exceptionCode != 0)
+		RaiseException(
+			(0x0FFFFFFF & exceptionCode) | 0xE0000000, 0, 0, NULL);
 
 	return;
 }
