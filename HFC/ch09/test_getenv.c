98bc58c6248c069412eb2f6961ddffe87cfe0e8c
earvinlin.02(mac14pronb) <earvinlin.02@gmail.com>
2025-07-03 14:11:39 +0800

    20250703-1411 add files.

--- HFC/ch09/test_getenv.c
+++ HFC/ch09/test_getenv.c
@@ -3,8 +3,8 @@
 
 int main(int argc, char *argv[])
 {
-    char *my_env[] = {"JUICE=peach and apple", NULL};
-    execle("diner_info", "diner_info", "4", NULL, my_env);
+//    char *my_env[] = {"JUICE=peach and apple", NULL};
+//    execle("diner_info", "diner_info", "4", NULL, my_env);
 
 	printf("Diners: %s\n", argv[1]);
 	printf("Juice: %s\n", getenv("JUICE"));
