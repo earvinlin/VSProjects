a80912459634034e760d18027874f891d58f3c34
earvinlin.02(macnb) <earvinlin.02@gmail.com(macnb)>
2025-07-09 22:03:12 +0800

    20250709-2203 add files.

--- HFC/ch09/coffee.c
+++ HFC/ch09/coffee.c
@@ -4,15 +4,15 @@
 int main(int argc, char *argv[])
 {
     char *w = getenv("EXTRA");
-    if (*w)
+    if (!w)
         w = getenv("FOOD");
-    if (*w)
+    if (!w)
         w = argv[argc - 1];
 
     char *c = getenv("EXTRA");
     if (!c)
         c = argv[argc - 1];
-    
+
     printf("%s with %s\n", c, w);
 
     return 0;
